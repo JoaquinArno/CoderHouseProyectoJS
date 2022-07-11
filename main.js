0c35de786ed535d79883b95ef10b65c2e59b0a33
Joaquin Arno <arnojoaquin17@gmail.com>
2022-07-11 14:56:43 -0300

    primerfuncion

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function ingresonombre () {
+    
+ let solicitarnombre1 = prompt("Ingresar el nombre del jugador 1 (Blancas)")
+ let solicitarnombre2 = prompt("Ingresar el nombre del jugador 2 (Negras)")
+
+}
+
+ingresonombre ();
